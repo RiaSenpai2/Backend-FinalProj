5d832393b62fb6836ee6bc14b27598c06ae3fc38
Ria Tyagi <riatyagi@Rias-MacBook-Air.local>
2023-12-09 09:27:36 -0500

    removed additional .env

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-console.log(process.env.STRIPE_SECRET_KEY); // In your Node.js server
+
 
 const Order = require('./models/Order'); // Make sure the path is correct
 
